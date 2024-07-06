f32efff9c1b9ffa1be430ee736194471e702236e
Matt Curtis <Curtismn87@yahoo.com>
2024-07-06 18:54:52 -0400

    adding netlify serverless

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'sb_ui';
+  name: string = '';
+  message: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  async fetchMessage(): Promise<void> {
+    const url = `/.netlify/functions/hello-world?name=${this.name}`;
+    try {
+      const response = await this.http.get<any>(url).toPromise();
+      this.message = response.message;
+    } catch (error) {
+      console.error('Error fetching message', error);
+    }
+  }
 }
